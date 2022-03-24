1cc0bc91220b09aa3336868da9130388326ddc90
Hhenrique-Gomes <henriquegomes99h@gmail.com>
2022-03-24 14:03:42 -0300

    Testando construtor 24/03/2022

---
+++ 51-Construtores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _51_Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o Nome do(a) construtor(a)");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite a idade do(a) construtor(a)");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o sexo do(a) construtor(a)");
+            char sexo = Convert.ToChar(Console.ReadLine());
+
+            maoobra j = new maoobra(nome,idade,sexo);
+
+            Console.WriteLine("Digite um numero para diminuir a idade");
+            int idde = Convert.ToInt32(Console.ReadLine());
+            j.sbtrc(idde);
+
+            Console.WriteLine("{0}",j.linha1());
+
+        }
+    }
+}
---
+++ 51-Construtores/maoobra.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _51_Construtores
+{
+    class maoobra
+    {
+
+        public string Nome;
+        public int Idade;
+        public char Sx;
+
+        public maoobra  (string nome, int idade, char sx)
+        {
+            Nome = nome;
+            Idade = idade;
+            Sx = sx;
+        }
+
+        public int sbtrc(int idde)
+        {
+            return Idade = Idade - idde;
+        }
+
+        public string linha1()
+        {
+            string linha;
+            Console.WriteLine("Nome: {0} \n" +
+                "Idade: {1} \n" +
+                "Sexo: {2}",Nome,Idade,Sx);
+            return linha = Console.ReadLine();
+            
+            
+        }
+    }
+}
